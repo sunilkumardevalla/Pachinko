2a966cacf85d1faa07dd2d91a20d5d515b7629da
SunilKumarDevalla <sunilkumardevalla@gmail.com>
2022-04-11 17:29:17 -0500

    Add fire sparkle for the ball.

--- Pachinko/Pachinko/GameScene.swift
+++ Pachinko/Pachinko/GameScene.swift
@@ -138,6 +138,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
     func destroy(ball: SKNode) {
+        if let fireParticles = SKEmitterNode(fileNamed: "FireParticles") {
+            fireParticles.position = ball.position
+            addChild(fireParticles)
+        }
         ball.removeFromParent()
     }
     
